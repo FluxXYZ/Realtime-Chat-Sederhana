1cb6239d554598fc1e7b1d7ddd88236996056c8a
FluxXYZ <greggbbot4@gmail.com>
2025-08-20 16:48:47 +0800

    ngubah

---
+++ api/firebase-config.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASEURL,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID,
+  });
+}
